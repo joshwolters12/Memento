b2355d6f915d2e8e477f4a9ec174ca2f0792b338
Joshua Wolters <joshuawolters@Joshuas-MacBook-Pro.local>
2017-08-08 17:27:54 -0700

    updated with navigation module

--- App.js
+++ App.js
@@ -1,26 +1,86 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  DrawerNavigator,
+  StackNavigator,
+  TabNavigator
+} from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Home from './src/tabs/Home';
+import Settings from './src/tabs/Settings';
+import Profile from './src/screens/Profile';
+import Modal from './src/screens/Modal';
+import Drawer from './src/components/Drawer';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-        <Text style={styles.text}>Changes you make will automatically reload.</Text>
-        <Text style={styles.text}>Shake your phone to open the developer menu. </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center'
+// Stack navigation for Settings and Profile screens
+const SettingsTab = StackNavigator({
+  Settings: {
+    screen: Settings,
+    navigationOptions: {
+      header: null,               // Hide the header
+      headerBackTitle: 'Back!!!!!',    // Title back button Back when we navigate to Profile from Settings
+    },
   },
-  text:{
-    color: 'white'
+  Profile: {
+    screen: Profile,
+    navigationOptions: ({ navigation }) => ({
+      // Customize header's title with user name passed to navigate()
+      // You can pass any props you'd like. For instance: navigate('Profile', { user: 'Tom' }
+      title: `${navigation.state.params.user}'s Profile`,
+    }),
+  },
+},
+ {
+  headerMode: 'screen', 
+});
+
+// Tab navigation for Home and Settings screens
+const TabNavigation = TabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: 'Homeyyy',
+      tabBarIcon: ({ tintColor, focused }) => <Icon
+        name={focused ? 'ios-home' : 'ios-home-outline'}
+        size={26}
+        style={{ color: tintColor }}
+      />
+    },
+  },
+  Settings: {
+    screen: SettingsTab,
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: ({ tintColor, focused }) => <Icon
+        name={focused ? 'ios-settings' : 'ios-settings-outline'}
+        size={26}
+        style={{ color: tintColor }}
+      />
+    },
+  },
+});
+
+// Wrap tab navigation into drawer navigation
+const TabsWithDrawerNavigation = DrawerNavigator({
+  Tabs: {
+    screen: TabNavigation,
   }
+}, {
+  // Register custom drawer component
+  contentComponent: props => <Drawer {...props} />
+});
+
+// And lastly stack together drawer with tabs and modal navigation
+// because we want to be able to call Modal screen from any other screen
+export default StackNavigator({
+  TabsWithDrawer: {
+    screen: TabsWithDrawerNavigation,
+  },
+  Modal: {
+    screen: Modal
+  },
+}, {
+  // In modal mode screen slides up from the bottom
+  mode: 'modal',
+  // No headers for modals. Otherwise we'd have two headers on the screen, one for stack, one for modal.
+  headerMode: 'none',
 });
